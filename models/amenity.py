4b2c30581c8c83f77431cd3eb591bee370e29c3c
Jeremiah3019 <raymmanson21@gmail.com>
2023-06-22 04:29:59 -0700

    Task 6. DBStorage - States and Cities

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the amenity class"""
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from models.place import place_amenity
-from sqlalchemy.orm import relationship
+
 
 class Amenity(BaseModel, Base):
-    '''amenity class'''
-    __tablename__ = 'amenities'
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity,
-                                    viewonly=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
